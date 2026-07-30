29ef434add71fedf34595b6ff26106a6d17399a9
Nitish kumar <nitishrahulchandan@gmail.com>
2026-07-30 23:48:18 +0530

    Time: 19 ms (56.17%) | Memory: 29.7 MB (37.55%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '*') {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } 
+            else {
+                st.push(ch);
+            }
+        }
+
+        string result;
+
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
