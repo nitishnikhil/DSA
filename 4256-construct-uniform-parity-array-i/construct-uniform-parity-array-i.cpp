f2f1c2f0463a6a4d98310456085deef3efda9d24
Nitish kumar <nitishrahulchandan@gmail.com>
2026-09-03 18:13:30 +0530

    Time: 2 ms (6.90%) | Memory: 30.2 MB (95.03%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+        //vector<int>nums2(n);
+
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]%2==0)
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]%2 !=0)
+        // }
+        //or any odd value with even value then aray become true;
+        return true;
+    }
+};
