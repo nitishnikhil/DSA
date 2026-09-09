656bc946a8f4485a97a4693c92d7f4989d59c9a9
Nitish kumar <nitishrahulchandan@gmail.com>
2026-09-09 17:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long result=0;
+        long long start=1000;
+        while(start<=n){
+            result +=(n-start+1);
+            start *=1000;
+        }
+        return result;
+    }
+};
